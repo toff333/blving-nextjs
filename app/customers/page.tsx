5a6b397b47d8183435ddeb404a227e7f2f6e678a
toff333 <>
2023-12-06 17:02:44 +1100

    first commit

---
+++ app/customers/page.tsx
@@ -0,0 +1,89 @@
+import { Customers, columns } from "./columns"
+import { DataTable } from "./customers-table"
+
+async function getData(): Promise<Customers[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "26e30f74-0d35-587f-8da0-173dd626b40c",
+      amount: 174,
+      status: "pending",
+      name: "Mayme",
+      email: "awumu@uga.jo",
+    },
+    {
+      id: "32bd8f0c-0a28-5c89-b27d-86c6c39e866a",
+      amount: 852,
+      status: "pending",
+      name: "Bruce",
+      email: "bihgeh@nigu.io",
+    },
+    {
+      id: "c5164a67-1ede-57f3-849a-975ee9b9e300",
+      amount: 570,
+      status: "pending",
+      name: "Eliza",
+      email: "uruna@siwsuto.mm",
+    },
+    {
+      id: "b1e85843-98aa-5aab-8fe9-fb98197bae12",
+      amount: 365,
+      status: "pending",
+      name: "Beatrice",
+      email: "zudtaapi@ehogupgi.ie",
+    },
+    {
+      id: "53c8ade1-63e3-5436-82e6-81b8d7511cfb",
+      amount: 958,
+      status: "pending",
+      name: "Cordelia",
+      email: "hormas@limo.vn",
+    },
+    {
+      id: "04cb94bc-a84c-5702-bb3a-bb76e8850c23",
+      amount: 624,
+      status: "pending",
+      name: "Ophelia",
+      email: "netehuz@vul.aw",
+    },
+    {
+      id: "3baeb2e0-2cf1-594b-b7c6-b183c3ceffe8",
+      amount: 451,
+      status: "pending",
+      name: "Jose",
+      email: "elop@kij.je",
+    },
+    {
+      id: "41971d23-73f6-544f-8fe9-26f04c2f59b8",
+      amount: 156,
+      status: "pending",
+      name: "Milton",
+      email: "emja@nabfah.th",
+    },
+    {
+      id: "c3e5b7bf-0d78-56cf-91e3-c46016f9ead0",
+      amount: 96.24,
+      status: "pending",
+      name: "Olivia",
+      email: "omcolbub@lup.ch",
+    },
+    {
+      id: "c93952bc-db89-54bf-8f23-513d60166048",
+      amount: 100,
+      status: "pending",
+      name: "Albert",
+      email: "ri@mavegoid.is",
+    },
+    // ...
+  ]
+}
+
+export default async function CustomersPage() {
+  const data = await getData()
+
+  return (
+    <div className="container mx-auto py-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  )
+}
---
+++ app/transactions/columns.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DataTableColumnHeader } from "@/components/column-header";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Transactions = {
+  id: string;
+  amount: number;
+  status: "pending" | "processing" | "success" | "failed";
+  name: string;
+  email: string;
+};
+
+export const columns: ColumnDef<Transactions>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Name" />
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => (
+      <DataTableColumnHeader column={column} title="Email" />
+    ),
+
+    // header: ({ column }) => {
+    //   return (
+    //     <Button
+    //       variant="ghost"
+    //       onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    //     >
+    //       Email
+    //       <ArrowUpDown className="ml-2 h-4 w-4" />
+    //     </Button>
+    //   );
+    // },
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "PHP",
+      }).format(amount);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const transaction = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(transaction.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View transaction details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+];
